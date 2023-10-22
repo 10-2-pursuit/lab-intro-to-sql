c23b1964c26394c6d98e3278612ff2d8ff804410
Nicole Slater <nicoleslater@pursuit.org>
2023-10-22 15:35:55 -0400

    Setting up table and adding gifts

--- regifter.sql
+++ regifter.sql
@@ -41,18 +41,24 @@ CREATE DATABASE regifter;
 \echo Uncomment below to insert 5 more gifts
 -- 
 
--- INSERT INTO gifts (gift, giver, value, previously_regifted)
--- VALUES
--- ('peach candle', 'Santa', '9', TRUE),
--- ('cinnamon candle', 'Nick', '19', TRUE),
--- ('soap on a rope', 'Rudolf', '29', FALSE),
--- ('potpurri', 'Elf on the Shelf', '39', TRUE),
--- ('mango candle', 'The Boss', '49', FALSE)
--- ;
+INSERT INTO gifts (gift, giver, value, previously_regifted)
+VALUES
+('peach candle', 'Santa', '9', TRUE),
+('cinnamon candle', 'Nick', '19', TRUE),
+('soap on a rope', 'Rudolf', '29', FALSE),
+('potpurri', 'Elf on the Shelf', '39', TRUE),
+('mango candle', 'The Boss', '49', FALSE)
+;
 
 -- 
 \echo Insert 5 more gifts of your own choosing,  include 1 more candle
---
+('gameboy', 'Mom', '100', FALSE),
+('puppy', 'Dad', '1000', FALSE),
+('sketchpad', 'Grandma', '10', TRUE), 
+('baby doll', 'Aunt', '25', TRUE), 
+('jigsaw puzzle', 'Santa', '5', FALSE), 
+('vanilla candle', 'Neighbor', '10', TRUE)
+
 
 
 
