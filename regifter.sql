57507de7351145404805a5f0d2351ecc5dadf03f
James Edmond <jamesedmond@pursuit.org>
2023-10-22 20:25:59 -0400

    Finished, did one bonus

--- regifter.sql
+++ regifter.sql
@@ -70,27 +70,27 @@ INSERT INTO gifts (gift, giver, value, previously_regifted)
 --
 \echo Query for gifts with a price greater than or equal to 20
 --
-
+SELECT * FROM gifts WHERE price >= 20
 
 --
 \echo Query for every gift that has the word candle in it, only show the gift column
 --
-
+SELECT * FROM gifts WHERE gift LIKE '%candle%';
 
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
 --
-
+regifter=# SELECT * FROM gifts WHERE giver = 'Santa' OR value > 30;
 
 --
 \echo Query for every gift whose giver is NOT Santa
 --
-
+SELECT * FROM gifts WHERE giver != 'Santa';
 
 --
 \echo Update the second gift to have a value of 2999
 -- 
-
+UPDATE gifts SET value = 2999 WHERE id = 2 RETURNING *;
 
 --
 \echo Query for the updated item
@@ -100,7 +100,7 @@ INSERT INTO gifts (gift, giver, value, previously_regifted)
 --
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
 --
-
+DELETE FROM gifts WHERE giver = 'Santa' RETURNING value, gift;
 
 --
 \echo Query for all the columns in your gifts table one more time
@@ -118,7 +118,7 @@ INSERT INTO gifts (gift, giver, value, previously_regifted)
 --
 \echo Get the AVEREAGE value from all the gifts
 --
-
+SELECT AVG(value) FROM gifts;
 
 -- 
  \echo Limit to 3 gifts, offset by 2 and order by price descending
